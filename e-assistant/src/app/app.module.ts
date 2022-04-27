2b231364cc37439fa25db4f97532c29fe91a85cf
Albert <a.escote@investel.es>
2022-04-27 19:30:06 +0200

    added nav-bar + login + register view (initial commit)

--- e-assistant/src/app/app.module.ts
+++ e-assistant/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, LoginComponent, RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ e-assistant/src/app/register/register.component.ts
@@ -0,0 +1,21 @@
+// register.component.ts
+
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.css"]
+})
+export class RegisterComponent {
+  email!: string;
+  password!: string;
+  confirmPassword!: string;
+
+  constructor() {}
+
+  register() {
+    console.log(this.email);
+    console.log(this.password);
+  }
+}
