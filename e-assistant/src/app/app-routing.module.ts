396f9747206b27a35366225b95ca584fd68120ff
Igantius Costa <ignatius.costa@basetis.com>
2022-05-17 15:49:57 +0200

    home page

--- e-assistant/src/app/app-routing.module.ts
+++ e-assistant/src/app/app-routing.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { LoginPasswordComponent } from "./login/login-password/login-password.component";
-import { LoginComponent } from "./login/login.component";
+import { LoginPasswordComponent } from './login/login-password/login-password.component';
+import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home-page/home.component';
 
 const routes: Routes = [
+  { path: 'login', component: LoginComponent, pathMatch: 'full' },
   { path: 'home', component: HomeComponent, pathMatch: 'full' },
-  { path: "login", component: LoginComponent, pathMatch: "full" },
-  { path: "login-password", component: LoginPasswordComponent, pathMatch: "full" },
-  { path: "register", component: RegisterComponent, pathMatch: "full" },
-  { path: '', pathMatch: 'full', redirectTo: '/home' }
+  {
+    path: 'login-password',
+    component: LoginPasswordComponent,
+    pathMatch: 'full',
+  },
+  { path: 'register', component: RegisterComponent, pathMatch: 'full' },
+  { path: '', pathMatch: 'full', redirectTo: '/home' },
 ];
 
 @NgModule({
