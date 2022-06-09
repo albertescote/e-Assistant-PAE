e7efd248380ad32feb5fee124acd34cf44573afd
Albert <a.escote@investel.es>
2022-06-09 20:21:53 +0200

    uploading uipath projects

--- e-assistant-Backend-API/API.py
+++ e-assistant-Backend-API/API.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 @app.route('/sync', methods = ['POST'])
 def sync():
-    os.system('./UiRobot.exe execute --file {path to UiPath DownloadPDF project}/project.json')
+    os.system('./UiRobot.exe execute --file C:/Users/alber/OneDrive/Documents/UPC/Q8/PAE/DownloadPDF/project.json')
 
 @app.route('/sign', methods = ['POST'])
 def sign():
-    os.system('./UiRobot.exe execute --file {path to UiPath SignPDF project}/project.json')
+    os.system('./UiRobot.exe execute --file C:/Users/alber/OneDrive/Documents/UPC/Q8/PAE/SignPDF/project.json')
 
 if __name__ == "__main__":
     app.run(debug=True)
